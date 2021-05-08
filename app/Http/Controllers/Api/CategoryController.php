131184f1e7bd4b850232b8e2757ba9d804744830
awalrizqifr <awalrizqifr@gmail.com>
2021-05-08 23:28:43 +0700

    Finishing CRUD: Update, Delete and Resource Controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -25,5 +27,19 @@ class CategoryController extends Controller
         $category = Category::create($request->all());
 
         return new CategoryResource($category);
+    }
+
+    public function update(Category $category, UpdateCategoryRequest $request)
+    {
+        $category->update($request->all());
+
+        return new CategoryResource($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
